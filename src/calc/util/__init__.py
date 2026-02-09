3b7a9582ee02d5fa365bc5e7fff27be0dca08bf1
Willow Ahrens <willow@csail.mit.edu>
2026-02-09 18:07:57 -0500

    remove donfig

--- src/calc/util/__init__.py
+++ src/calc/util/__init__.py
@@ -1,5 +1,3 @@
-from .cache import file_cache
-from .config import config, get_version
 from .print import qual_str
 
-__all__ = ["config", "file_cache", "get_version", "qual_str"]
+__all__ = ["qual_str"]
